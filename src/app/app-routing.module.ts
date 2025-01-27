9fb43eac78253e1d8b52b440a8dbcc6a772d3f72
Tu Nombre <tuemail@example.com>
2025-01-27 11:31:35 -0600

    oa

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'configuracion',
+    loadChildren: () => import('./configuracion/configuracion.module').then( m => m.ConfiguracionPageModule)
+  },
+  {
+    path: 'llamadas',
+    loadChildren: () => import('./llamadas/llamadas.module').then( m => m.LlamadasPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/llamadas/llamadas.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-llamadas',
+  templateUrl: './llamadas.page.html',
+  styleUrls: ['./llamadas.page.scss'],
+  standalone: false,
+})
+export class LlamadasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
